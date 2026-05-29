7bedccf36014fa88bc79f1cbc7dc461a8c19ca56
FahadC <fsachaudhary@gmail.com>
2026-05-29 23:39:39 +0100

    decided to use complementary filter instead of kalman

--- accelCode/test.cpp
+++ accelCode/test.cpp
@@ -1,33 +1,67 @@
 // test_mpu6050.cpp
 // Compile with: g++ test_mpu6050.cpp -o test_mpu6050 -li2c
 
+#define _USE_MATH_DEFINES
 #include "MPU6050.h"
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
 
 int main() {
     // 0x68 is the default I2C address (AD0 pin LOW)
     // Use 0x69 if AD0 is pulled HIGH
     MPU6050 mpu(0x68);
 
-    std::cout << "MPU6050 Tester - Raw Accelerometer & Gyroscope Data" << std::endl;
-    std::cout << "=====================================================" << std::endl;
+    cout << "MPU6050 Tester - Raw Accelerometer & Gyroscope Data" << endl;
+    cout << "=====================================================" << endl;
 
-    float ax, ay, az;
-    float gx, gy, gz;
-
+    double ax, ay, az;
+    double gx, gy, gz;
+    double complementaryAngle = 0.0;
+    
     while (true) {
         mpu.getAccel(&ax, &ay, &az);
         mpu.getGyro(&gx, &gy, &gz);
 
-        std::cout << "\n--- Accelerometer (g) ---" << std::endl;
-        std::cout << "  X: " << ax << std::endl;
-        std::cout << "  Y: " << ay << std::endl;
-        std::cout << "  Z: " << az << std::endl;
+	
+	// Get accelorometer measurements (may need to lowpass this)
+	cout << "\n--- Accelerometer (g) ---" << endl;
+        cout << "  X: " << ax << endl;
+        cout << "  Y: " << ay << endl;
+        cout << "  Z: " << az << endl;
 
-        std::cout << "--- Gyroscope (deg/s) ---" << std::endl;
-        std::cout << "  Roll:  " << gx << std::endl;
-        std::cout << "  Pitch: " << gy << std::endl;
-        std::cout << "  Yaw:   " << gz << std::endl;
+	// Calculate the accelorometer angle presumably through trig
+	double accelAngleY = atan2(ay/sqrt((ax*ax) + (az*az))) * 180 / M_PI;
+        cout << accelAngleY << endl;
+	
+	// Mutliply the final result by the weight you assign
+	double accelWeight = 0.01;
+	double finalAccelAngleY = accelAngleY * accelWeight; 
+	
+	
+	// get gyroscope measurements
+        cout << "--- Gyroscope (deg/s) ---" << endl;
+        cout << "  Roll:  " << gx << endl;
+        cout << "  Pitch: " << gy << endl;
+        cout << "  Yaw:   " << gz << endl;
 
+
+	// multiply by delta time, whatever that may be
+        double dt = 0.1;
+	double gyroPitchInit = gy * dt;
+	
+	// add the value of complementary angle (initially zero) to the result
+	double gyroPitch2 = gyroPitchInit + complementaryAngle;
+	
+	// multiply the result by the gyro weight
+	double gyroWeight = 0.99;
+	double gyroPitchFinal = gyroPitch2 * gyroWeight;
+	
+	// add the accel values and the gyro values together
+        double complementaryAngle = gyroPitchFinal + finalAccelAngleY;
+	
         usleep(100000); // 100ms (~10Hz)
     }
 

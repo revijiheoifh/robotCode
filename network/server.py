de6ea0ec6c4697229112d9b996320d05a9fe6409
revijiheoifh <fsachaudhary@gmail.com>
2026-07-31 21:14:48 +0100

    changed ip addr to get device ip

--- network/server.py
+++ network/server.py
@@ -1,6 +1,6 @@
 import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 12345))
+server_socket.bind((socket.gethostname(), 12345))
 server_socket.listen(1)
 print("Listening on port 12345...")
 conn, addr = server_socket.accept()
